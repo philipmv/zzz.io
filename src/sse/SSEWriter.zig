ec5c6ab1376ed3343ac89788318824960080bfcb
philipmv <31389342+philipmv@users.noreply.github.com>
2026-05-13 20:06:45 +0300

    optimize SSEWriter

--- src/sse/SSEWriter.zig
+++ src/sse/SSEWriter.zig
@@ -7,6 +7,7 @@ prefix: []const u8,
 cont: bool,
 
 const Self = @This();
+const splat_buffer_size = 64;
 
 pub fn init(writer: *Writer, buf: []u8, header: []const u8, dataprefix: []const u8) Writer.Error!Self {
     if (header.len > 0)
@@ -43,18 +44,45 @@ fn drain(w: *Writer, data: []const []const u8, splat: usize) Writer.Error!usize 
     const self: *@This() = @fieldParentPtr("interface", w);
     const buffered = w.buffered();
     var n: usize = 0;
+
     if (buffered.len > 0)
         n = try self.write(buffered);
+
     for (data[0 .. data.len - 1]) |d| {
         if (d.len == 0) continue;
         n += try self.write(d);
     }
+
     const pattern = data[data.len - 1];
-    if (splat > 0 and pattern.len > 0) {
-        for (0..splat) |_| {
-            n += try self.write(pattern);
-        }
+    switch (splat) {
+        0 => {},
+        1 => {
+            if (pattern.len > 0)
+                n += try self.write(pattern);
+        },
+        else => switch (pattern.len) {
+            0 => {},
+            1 => {
+                var splat_backup_buffer: [splat_buffer_size]u8 = undefined;
+                const splat_buffer = &splat_backup_buffer;
+                const memset_len = @min(splat_buffer.len, splat);
+                const buf = splat_buffer[0..memset_len];
+                @memset(buf, pattern[0]);
+                n += try self.write(buf);
+                var remaining_splat = splat - buf.len;
+                while (remaining_splat > splat_buffer.len) {
+                    std.debug.assert(buf.len == splat_buffer.len);
+                    n += try self.write(splat_buffer);
+                    remaining_splat -= splat_buffer.len;
+                }
+                n += try self.write(splat_buffer[0..@min(remaining_splat, splat_buffer.len)]);
+            },
+            else => for (0..splat) |_| {
+                n += try self.write(pattern);
+            },
+        },
     }
+
     return w.consume(n);
 }
 
